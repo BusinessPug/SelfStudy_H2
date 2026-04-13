484818e33adce5bdc06aad26d739b2933a30f614
Nikolaj <nikolaj.hering.hansen@gmail.com>
2026-04-13 12:16:57 +0200

    Init commit / Milestone 2

---
+++ SelfStudy_H2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace SelfStudy_H2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
